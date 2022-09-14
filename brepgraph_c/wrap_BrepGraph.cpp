2d8ae93af8b1eb9b720cae0b15a07948abe46168
zz <xzrunner@gmail.com>
2022-09-15 07:37:09 +0800

    add time

--- brepgraph_c/wrap_BrepGraph.cpp
+++ brepgraph_c/wrap_BrepGraph.cpp
@@ -60,7 +60,44 @@ void w_RTree_insert()
     for (auto& p : pts) 
     {
         auto src = p->pos.xyz;
-        const double dst[3] = { src[0], src[1], src[2] };
+        const double dst[4] = { src[0], src[1], src[2], 0 };
+        aabb.Combine(brepdb::Point(dst));
+    }
+
+    rtree->InsertData(length, data, aabb, id);
+    delete[] data;
+
+    ves_pop(ves_argnum());
+
+    auto rkey = std::make_shared<brepgraph::BRepKey>();
+    rkey->r = aabb;
+    rkey->id = id;
+
+    ves_pushnil();
+    ves_import_class("brepgraph", "RKey");
+    auto proxy = (tt::Proxy<brepgraph::BRepKey>*)ves_set_newforeign(0, 1, sizeof(tt::Proxy<brepgraph::BRepKey>));
+    proxy->obj = rkey;
+    ves_pop(1);
+}
+
+void w_RTree_insert_with_time()
+{
+    auto rtree = ((tt::Proxy<brepdb::RTree>*)ves_toforeign(0))->obj;
+    auto poly = ((tt::Proxy<pm3::Polytope>*)ves_toforeign(1))->obj;
+    double time = ves_tonumber(2);
+
+    uint8_t* data = nullptr;
+    uint32_t length = 0;
+    brepgraph::BRepTrans::BRepToByteArray(*poly, &data, length);
+
+    id_type id = 0;
+
+    brepdb::Region aabb;
+    auto& pts = poly->Points();
+    for (auto& p : pts) 
+    {
+        auto src = p->pos.xyz;
+        const double dst[4] = { src[0], src[1], src[2], time };
         aabb.Combine(brepdb::Point(dst));
     }
 
@@ -87,6 +124,52 @@ void w_RTree_query()
     
     auto visitor = std::make_unique<brepdb::ObjVisitor>();
     rtree->IntersectsWithQuery(key->r, *visitor);
+
+    std::vector<pm3::PolytopePtr> polys;
+
+    auto& items = visitor->GetResults();
+    for (auto item : items)
+    {
+        uint32_t len = 0;
+        uint8_t* data = nullptr;
+        item->GetData(len, &data);
+
+        auto poly = brepgraph::BRepTrans::BRepFromByteArray(data);
+        delete[] data;
+
+        polys.push_back(poly);
+    }
+
+    ves_pop(ves_argnum());
+
+    const int num = (int)(polys.size());
+    ves_newlist(num);
+    for (int i = 0; i < num; ++i)
+    {
+        ves_pushnil();
+        ves_import_class("geometry", "Polytope");
+        auto proxy = (tt::Proxy<pm3::Polytope>*)ves_set_newforeign(1, 2, sizeof(tt::Proxy<pm3::Polytope>));
+        proxy->obj = polys[i];
+        ves_pop(1);
+        ves_seti(-2, i);
+        ves_pop(1);
+    }
+}
+
+void w_RTree_query_with_time()
+{
+    auto rtree = ((tt::Proxy<brepdb::RTree>*)ves_toforeign(0))->obj;
+    auto key = ((tt::Proxy<brepgraph::BRepKey>*)ves_toforeign(1))->obj;
+
+    double tmin = ves_tonumber(2);
+    double tmax = ves_tonumber(3);
+
+    brepdb::Region r = key->r;
+    const_cast<double&>(r.GetLow()[3]) = tmin;
+    const_cast<double&>(r.GetHigh()[3]) = tmax;
+
+    auto visitor = std::make_unique<brepdb::ObjVisitor>();
+    rtree->IntersectsWithQuery(r, *visitor);
 
     std::vector<pm3::PolytopePtr> polys;
 
@@ -153,8 +236,8 @@ void w_RTree_query_leaves()
     auto box = ((tt::Proxy<gs::Box>*)ves_toforeign(1))->obj;
 
     const auto& c = box->GetCube();
-    const double min[] = { c.xmin, c.ymin, c.zmin };
-    const double max[] = { c.xmax, c.ymax, c.zmax };
+    const double min[] = { c.xmin, c.ymin, c.zmin, 0 };
+    const double max[] = { c.xmax, c.ymax, c.zmax, 0 };
     brepdb::Region region(min, max);
 
     brepgraph::RegionVisitor visitor;
@@ -189,8 +272,8 @@ void w_RKey_allocate()
     if (num == 2)
     {
         sm::cube cube = ((tt::Proxy<gs::Box>*)ves_toforeign(1))->obj->GetCube();
-        const double min[3] = { cube.xmin, cube.ymin, cube.zmin };
-        const double max[3] = { cube.xmax, cube.ymax, cube.zmax };
+        const double min[4] = { cube.xmin, cube.ymin, cube.zmin, 0 };
+        const double max[4] = { cube.xmax, cube.ymax, cube.zmax, 0 };
         key->r.Combine(brepdb::Point(min));
         key->r.Combine(brepdb::Point(max));
     }
@@ -229,7 +312,9 @@ namespace brepgraph
 VesselForeignMethodFn BrepGraphBindMethod(const char* signature)
 {
     if (strcmp(signature, "RTree.insert(_)") == 0) return w_RTree_insert;
+    if (strcmp(signature, "RTree.insert_with_time(_,_)") == 0) return w_RTree_insert_with_time;
     if (strcmp(signature, "RTree.query(_)") == 0) return w_RTree_query;
+    if (strcmp(signature, "RTree.query_with_time(_,_,_)") == 0) return w_RTree_query_with_time;
     if (strcmp(signature, "RTree.get_all_leaves()") == 0) return w_RTree_get_all_leaves;
     if (strcmp(signature, "RTree.query_leaves(_)") == 0) return w_RTree_query_leaves;
 
